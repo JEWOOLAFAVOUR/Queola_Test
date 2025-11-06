73c32cd05d066a4ace7789eb26e122c0baa1c25d
JEWOOLAFAVOUR <jewoolafavour2020@gmail.com>
2025-11-06 09:32:32 +0100

    feat: update Wolffins screen layout and improve text styling

--- src/screen/Wolffins.tsx
+++ src/screen/Wolffins.tsx
@@ -1,7 +1,7 @@
 import AntDesign from "@expo/vector-icons/AntDesign";
 import Feather from "@expo/vector-icons/Feather";
 import React from "react";
-import { StatusBar, StyleSheet, Text, View } from "react-native";
+import { Image, StatusBar, StyleSheet, Text, View } from "react-native";
 import { COLORS, FONTS, SIZES } from "../constants";
 
 const Wolffins = () => {
@@ -14,9 +14,10 @@ const Wolffins = () => {
           flexDirection: "row",
           alignItems: "center",
           justifyContent: "space-between",
+          marginBottom: SIZES.h4,
         }}
       >
-        <Text style={{ ...FONTS.h3, color: COLORS.black }}>Wolf Olins</Text>
+        <Text style={{ ...FONTS.h2, color: COLORS.black }}>Wolf Olins</Text>
 
         <View style={{ flexDirection: "row", alignItems: "center" }}>
           <AntDesign
@@ -27,6 +28,38 @@ const Wolffins = () => {
           />
           <Feather name="search" size={17} color="black" />
         </View>
+      </View>
+      <View>
+        <Image
+          source={{
+            uri: "https://www.wolffolins.com/_next/image?url=https%3A%2F%2Fimages.ctfassets.net%2F4rnblstkg79m%2F18EG6DImEQoL7MiuPoPPLm%2Fcec721ff4fe2283746e5e8ea12022d1a%2F00_Lloyds_Website_Carousel.jpg&w=1920&q=75",
+          }}
+          style={{ width: SIZES.width, height: SIZES.height * 0.4 }}
+        />
+      </View>
+      <View
+        style={{ paddingTop: SIZES.h4, paddingHorizontal: SIZES.width * 0.04 }}
+      >
+        <Text
+          style={{
+            ...FONTS.h2,
+            color: COLORS.black,
+            fontFamily: "Inter-Regular",
+            marginBottom: SIZES.h5,
+          }}
+        >
+          Our Ambition
+        </Text>
+        <Text
+          style={{
+            ...FONTS.body1,
+            color: COLORS.black,
+            fontFamily: "Inter-Medium",
+          }}
+        >
+          We create transformative brands that move businesses, people and the
+          world forward.
+        </Text>
       </View>
     </View>
   );
